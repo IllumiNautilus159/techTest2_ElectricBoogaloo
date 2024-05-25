af87e36542f4fa591d813360f2d3436abf4b5171
IllumiNautilus159 <75284467+IllumiNautilus159@users.noreply.github.com>
2024-05-25 00:13:16 +0000

    auth route

--- src/server/api/[...auth].ts
+++ src/server/api/[...auth].ts
@@ -7,7 +7,6 @@ import {
 } from "next-auth";
 import { type Adapter } from "next-auth/adapters";
 import GitHubProvider from "next-auth/providers/github";
-import { env } from "~/env";
 import { db } from "~/server/db";
 
 /**
@@ -39,13 +38,13 @@ declare module "next-auth" {
 export const authOptions: NextAuthOptions = {
 
   callbacks: {
-    session: ({ session, user }) => ({
-      ...session,
-      user: {
-        ...session.user,
-        id: user.id,
-      },
-    }),
+    // session: ({ session, user }) => ({
+    //   ...session,
+    //   user: {
+    //     ...session.user,
+    //     id: user.id,
+    //   },
+    // }),
   },
   adapter: PrismaAdapter(db) as Adapter,
   providers: [
