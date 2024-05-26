8b676eac405f9432545e9435b18db2c1811e9afc
IllumiNautilus159 <75284467+IllumiNautilus159@users.noreply.github.com>
2024-05-26 02:14:36 +0000

    migrate with router

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,9 @@
 import NextAuth from "next-auth";
+import { NextRouter } from "next/router";
 
 import { authOptions } from "~/server/api/auth";
 
 
-const handler = NextAuth(authOptions);
+const handler : NextRouter = NextAuth(authOptions);
 export { handler as GET, handler as POST };
 export default handler;
