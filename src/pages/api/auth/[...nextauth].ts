4abd9aadb8b3425cdd5b5d2c18469b0c33153dfb
IllumiNautilus159 <75284467+IllumiNautilus159@users.noreply.github.com>
2024-05-26 02:18:33 +0000

    oauth routes

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,9 +1,5 @@
 import NextAuth from "next-auth";
-import { NextRouter } from "next/router";
 
 import { authOptions } from "~/server/api/auth";
 
-
-const handler : NextRouter = NextAuth(authOptions);
-export { handler as GET, handler as POST };
-export default handler;
+export default NextAuth(authOptions);
